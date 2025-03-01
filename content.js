fc3d2e62d17bd4900eeb3500823cb05e6d5fccf6
Zafir Sk Heerah <zafir.heerah@spoonconsulting.com>
2025-03-01 17:08:40 +0400

    Enable/Disable Extension

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('injected.js');
-(document.head || document.documentElement).appendChild(script);
-script.onload = function() {
-  script.remove();
-};
+chrome.storage.local.get(['extensionEnabled'], function(result) {
+  if (result.extensionEnabled === false) {
+    console.log('Limitless Textarea extension is disabled.');
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.src = chrome.runtime.getURL('injected.js');
+  (document.head || document.documentElement).appendChild(script);
+  script.onload = function() {
+    script.remove();
+  };
+});
